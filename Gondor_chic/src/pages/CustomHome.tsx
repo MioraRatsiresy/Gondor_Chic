5b83770a56338ba20df0ff808b5dcc4be2ed8732
LuckaRouane <99262458+LuckaRouane@users.noreply.github.com>
2024-06-13 14:15:57 +0300

    CustomHome

--- Gondor_chic/src/pages/CustomHome.tsx
+++ Gondor_chic/src/pages/CustomHome.tsx
@@ -33,27 +33,28 @@ const CustomHome: React.FC<ProductProps> = ({ productName, description, stock, p
     };
 
     return (
-        <div className="product-container">
-            <img src={imageUrl} alt={productName} className="product-image" />
-            <div className="product-details">
-                <h2 className="product-title">{productName}</h2>
-                <p>Bonjour, <strong className="product-greeting">{user.first_name} {user.last_name}</strong></p>
-                <p className="product-description">{description}</p>
-                <p className="product-stock"><strong>Stock:</strong> {stock} | <strong>{price} $</strong></p>
-                <div className="quantity-and-button">
+        <div className="custom-home-product-container">
+            <img src={imageUrl} alt={productName} className="custom-home-product-image" />
+            <div className="custom-home-product-details">
+                <h2 className="custom-home-product-title">{productName}</h2>
+                <p>Bonjour, <strong className="custom-home-product-greeting">{user.first_name} {user.last_name}</strong></p>
+                <p className="custom-home-product-description">{description}</p>
+                <br />
+                <p className="custom-home-product-stock"><strong>Stock:</strong> {stock} | <strong>{price} $</strong></p>
+                <div className="custom-home-quantity-and-button">
                     <input 
                         type="number" 
                         value={quantity} 
                         min="1" 
                         max={stock} 
                         onChange={handleQuantityChange} 
-                        className="quantity-input"
+                        className="custom-home-quantity-input"
                     />
-                    <button onClick={handleAddToCart} className="add-to-cart-button">
+                    <button onClick={handleAddToCart} className="custom-home-add-to-cart-button">
                         AJOUTER AU PANIER
                     </button>
                 </div>
-                {quantity > stock && <p className="stock-warning">Le stock est limité, veuillez vérifier la quantité</p>}
+                {quantity > stock && <p className="custom-home-stock-warning">Le stock est limité, veuillez vérifier la quantité</p>}
             </div>
         </div>
     );
